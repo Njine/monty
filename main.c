93767e74956971e8f7825ac799caf7f95667c057
njine <brikab@gmail.com>
2023-09-25 04:26:07 +0300

    globe

--- main.c
+++ main.c
@@ -8,38 +8,38 @@ bus_t bus = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
+	char *stuff;
+	FILE *folder;
+	size_t vast = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty folder\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	folder = fopen(argv[1], "r");
+	bus.file = folder;
+	if (!folder)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read_line > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
+		stuff = NULL;
+		read_line = getline(&stuff, &vast, folder);
+		bus.content = stuff;
 		counter++;
 		if (read_line > 0)
 		{
-			execute(content, &stack, counter, file);
+			execute(stuff, &stack, counter, folder);
 		}
-		free(content);
+		free(stuff);
 	}
 	free_stack(stack);
-	fclose(file);
+	fclose(folder);
 return (0);
 }
--- prints.c
+++ prints.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 
 /**
- * f_pall - It prints the stack
- * @head: Pointer to the head of stack
- * @counter: Unused
- * Return: No return value
- */
+* f_pall - It prints the stack
+* @head: Pointer to the head of stack
+* @counter: Unused
+* Return: No return value
+*/
 void f_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *h;
+stack_t *h;
 
-    (void)counter;
+(void)counter;
 
-    h = *head;
-    if (h == NULL)
-        return;
+h = *head;
+if (h == NULL)
+return;
 
-    for (; h != NULL; h = h->next)
-    {
-        printf("%d\n", h->n);
-    }
+for (; h != NULL; h = h->next)
+{
+printf("%d\n", h->n);
+}
 }
